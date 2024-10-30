b4e1cf22ae0bbf3b8127dd0e9432fdbb60b33aa2
varun.nayak@niveussolutions.com <varun.nayak@niveussolutions.com>
2024-10-30 10:18:16 +0530

    Initial Commit Assignment 1

---
+++ src/main/java/com/nivieus/test_apis/TestApisApplication.java
@@ -0,0 +1,13 @@
+package com.nivieus.test_apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApisApplication.class, args);
+	}
+
+}
